bee7d61c5ce3f29906bb8c6fe13c0a24004b2c3c
Poloche <sebastianpolochetellez@gmail.com>
2026-02-09 19:11:18 -0500

    docs: cambios en documentacion

--- ejercicio1.py
+++ ejercicio1.py
@@ -4,13 +4,19 @@ def analizar_numeros(lista_numeros):
     Analiza una lista de numero enteros y retorna estadisticas basicas
 
     Args:
-        lista_numeros (list): Lista de números enteros a analizar
+        lista_numeros (list): Lista de numeros enteros a analizar
     
     Returns:
-        tuple: (pares, impares, promedio, numero_mayor, numero_menor)
+    dict: Diccionario con estadísticas o mensaje de error
+        - pares (int): Cantidad de números pares
+        - impares (int): Cantidad de números impares
+        - promedio (float): Promedio de los números
+        - numero_mayor (int): Número más grande
+        - numero_menor (int): Número más pequeño
     """
+
     if len(lista_numeros) == 0:
-        return {"error": "La lista no puede estar vacía"}
+        return {"error": "La lista no puede estar vacia"}
 
     contador_pares = 0
     contador_impares = 0
